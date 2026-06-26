cbd061b5be241fc8b9018350e9f1e4b1c646faec
Subhadeep <Subhadeeppodder08gmail.com>
2026-06-26 12:07:53 +0530

    new changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-q$-8l03m1e^fj)zn)y2sw3^i!lw)#1+020=cx&a$)y*ie_=3^*"
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["localhost",
+                "127.0.0.1",
+                ".onrender.com"]
 
 
 # =========================================================
